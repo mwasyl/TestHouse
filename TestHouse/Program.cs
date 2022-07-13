c5537f12e3371c1eb1cf15b3b6d3032cf99f1eb3
Marcin <mwasyl@gmail.com>
2022-07-13 13:22:52 +0200

    Nowy projekt

---
+++ TestHouse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

d3c982e2ac9cd0a1ba230e44e8c2a79d162838bb
Marcin <mwasyl@gmail.com>
2022-07-13 13:29:16 +0200

    Cos dodalem

--- TestHouse/Program.cs
+++ TestHouse/Program.cs
@@ -7,6 +7,8 @@ namespace TestHouse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Wojtek!");
+
         }
     }
 }
